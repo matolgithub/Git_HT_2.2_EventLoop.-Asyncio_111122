83d384f95ab5268cf5e76e9469d9fbc76e98457d
matolgithub <89175955650@mail.ru>
2022-11-12 19:55:52 +0300

    Next commit: made list_names_ersons in sync_swapi.py.

--- sync_swapi.py
+++ sync_swapi.py
@@ -1,22 +1,32 @@
 import requests
 from pprint import pprint
 import datetime
+import math
 
-url = "https://swapi.dev/api/people/"
+url = "https://swapi.dev/api/people"
 list_names_persons = []
+start_time = datetime.datetime.now()
 
 response = requests.get(url=url)
-start_time = datetime.datetime.now()
-list_persons = [item for item in response.json()["results"]]
-for item in list_persons:
-    for key, value in item.items():
-        if key == "name":
-            list_names_persons.append(value)
+
+total_quant_persons = response.json()["count"]
+total_num_pages = math.ceil(total_quant_persons / 10)
+
+for num_page in range(1, total_num_pages + 1):
+    if num_page > 1:
+        url = f"https://swapi.dev/api/people/?page={num_page}"
+        response = requests.get(url=url)
+    list_persons = [item for item in response.json()["results"]]
+    for item in list_persons:
+        for key, value in item.items():
+            if key == "name":
+                list_names_persons.append(value)
 
 total_time = datetime.datetime.now() - start_time
 
-print(f"{total_time}\n{len(list_names_persons)}\n{list_names_persons}")
+print(f"Executed time: {total_time}\nTotal persons: {total_quant_persons}\nIncluding in list persons:"
+      f" {len(list_names_persons)}\nList of persons: {sorted(list_names_persons)}")
 
 # pprint(response.json())
 # print(response.status_code)
-# print(response.text)
+# pprint(response.text)
