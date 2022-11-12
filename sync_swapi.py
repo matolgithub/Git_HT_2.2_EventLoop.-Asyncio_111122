23bbc4a85f721f751a4665171303f1a30cb451fd
matolgithub <89175955650@mail.ru>
2022-11-12 23:40:18 +0300

    Commit: made def get_dict_persons(self) in sync_swapi.py.

--- sync_swapi.py
+++ sync_swapi.py
@@ -34,16 +34,60 @@ class Swapi:
         return list_names_persons
 
     # create dictionary of Star Wars persons
-    # def get_dict_persons(self):
-    #     dict_persons = {}
-    #     start_time = datetime.datetime.now()
-    #     for item in range(1, total_quant_persons + 1):
-    #         url = f"{url}/{item}"
-    #         response = requests.get
-    #
-    #     total_time = datetime.datetime.now() - start_time
+    def get_dict_persons(self):
+        dict_persons = {}
+        response = requests.get(url=self.url)
+        total_quant_persons = response.json()["count"]
+        start_time = datetime.datetime.now()
+        item = 1
+        while len(dict_persons) < 3:  # change: total_quant_persons!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            dict_items = {}
+            url = f"{self.url}/{item}"
+            response = requests.get(url=url).json()
+            for key, value in response.items():
+                if value != "Not found":
+                    if key != "created" and key != "edited":
+
+                        if key == "films":
+                            films_string = ""
+                            for film_link in value:
+                                film_response = requests.get(url=film_link).json()["title"]
+                                films_string += f"{film_response}, "
+                                dict_items[key] = films_string[:-2]
+
+                        elif key == "species":
+                            species_string = ""
+                            for species_link in value:
+                                species_response = requests.get(url=species_link).json()["name"]
+                                species_string += f"{species_response}, "
+                                dict_items[key] = species_string[:-2]
+
+                        elif key == "starships":
+                            starships_string = ""
+                            for starships_link in value:
+                                starships_response = requests.get(url=starships_link).json()["name"]
+                                starships_string += f"{starships_response}, "
+                                dict_items[key] = starships_string[:-2]
+
+                        elif key == "vehicles":
+                            vehicles_string = ""
+                            for vehicles_link in value:
+                                vehicles_response = requests.get(url=vehicles_link).json()["name"]
+                                vehicles_string += f"{vehicles_response}, "
+                                dict_items[key] = vehicles_string[:-2]
+
+                        else:
+                            dict_items[key] = value
+                    dict_persons[int(f"{item}")] = dict_items
+            item += 1
+
+        pprint(dict_persons)
+        total_time = datetime.datetime.now() - start_time
+        print(total_time)
+
+        return dict_persons
 
 
 if __name__ == "__main__":
     swapi_obj = Swapi()
-    swapi_obj.get_list_persons()
+    swapi_obj.get_dict_persons()
