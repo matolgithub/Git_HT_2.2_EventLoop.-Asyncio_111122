7f6e12495667e9ff799672251841e4bdeabde028
matolgithub <89175955650@mail.ru>
2022-11-15 14:25:14 +0300

    Commit: add main, get_persons, upload_people in async_with_coroutines.py.

--- async_with_coroutines.py
+++ async_with_coroutines.py
@@ -1,9 +1,7 @@
-import requests
 import asyncio
 import datetime
 import asyncpg
 from more_itertools import chunked
-import math
 from aiohttp import ClientSession
 from pprint import pprint
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -16,42 +14,71 @@ from async_swapi import get_qty_persons
 from model import SwapiPeople
 
 load_dotenv()
+
 total_quant_persons = get_qty_persons()
+
 url = "https://swapi.dev/api/people"
+
 PG_DSN = getenv("PG_DSN")
+
+CHUNK_SIZE = getenv("CHUNK_SIZE")
+
 Base = declarative_base()
-
 engine = create_async_engine(PG_DSN)
+Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def get_person():
-    pass
+async def chunked_async(async_iter, size):
+    buffer_chunk = []
+    while True:
+        try:
+            item = await async_iter.__anext__()
+        except StopAsyncIteration:
+            break
+        buffer_chunk.append(item)
+        if len(buffer_chunk) == size:
+            yield buffer_chunk
+            buffer_chunk = []
+
+
+async def get_person(people_id: int, session: ClientSession):
+    print(f"Begin: {people_id}")
+    async with session.get(f"{url}/{people_id}") as response:
+        json_person_data = await response.json()
+    print(f"End: {people_id}")
+
+    return json_person_data
 
 
 async def get_persons():
-    pass
+    async with ClientSession() as session:
+        for chunk in chunked(range(1, total_quant_persons + 1), CHUNK_SIZE):
+            coroutines = [get_person(people_id=item, session=session) for item in chunk]
+            results = await asyncio.gather(*coroutines)
+            for person_data in results:
+                yield person_data
 
 
-async def db_upload():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        await conn.commit()
-    Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-    async for item in get_persons():
-        async with Session() as session:
-            session.add(item)
-            await session.commit()
-    print("Db - success!")
+async def upload_people(people_chunk):
+    async with Session() as session:
+        session.add_all([SwapiPeople(json=item) for item in people_chunk])
+        await session.commit()
 
 
 async def main():
-    task_get_api_persons = asyncio.create_task(get_persons())
-    await task_get_api_persons
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        await conn.commit()
 
-    task_upload_db = asyncio.create_task(db_upload())
-    await task_upload_db
+    async for chunk in chunked_async(get_persons(), CHUNK_SIZE):
+        asyncio.create_task(upload_people(chunk))
+
+    tasks = set(asyncio.all_tasks()) - {asyncio.current_task()}
+    for task in tasks:
+        await task
 
 
-# with asyncio: 3pers. - ___sec, 82pers. - ___sec.
+start_time = datetime.datetime.now()
 asyncio.run(main())
+total_time = datetime.datetime.now() - start_time
+print(f"Executed total time: {total_time}")
